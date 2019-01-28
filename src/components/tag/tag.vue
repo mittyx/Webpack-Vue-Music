2b8a074cd160a743d09bfdb7297da7ceed9471d7
a851870 <851870124@qq.com>
2019-01-28 12:45:18 +0800

    tag add timeEvent

--- src/components/tag/tag.vue
+++ src/components/tag/tag.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="tag">
-        <span><slot></slot></span>
+        <span @touchstart="onTouchStart"
+                @touchend="onTouchEnd">
+                <slot></slot>
+        </span>
     </div>
 </template>
 
@@ -9,7 +12,34 @@ export default {
     name: 'tag',
     data() {
         return {
+            timeEvent: null
+        }
+    },
+    methods: {
+        // 长按事件
+        onTouchStart() {
+            this.timeEvent = setTimeout(() => {
+                console.log('我被触发了')
+            }, 800);
+        },
+        onTouchEnd() {
+            clearTimeout(this.timeEvent)
         }
     }
 }
 </script>
+<style lang="scss" scoped>
+.tag{
+    height: 34px;
+    padding: 0 16px;
+    background-color: #DCDCDC;
+    border-radius:17.5px;
+    box-sizing: border-box;
+    line-height: 34px;
+    text-align: center;
+    span{
+        font-size: 14px;
+    }
+}
+</style>
+
